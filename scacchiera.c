3bb26dd28b062f584320bbaefb938471aabaa1ef
Davide Sirico <34192863+DavideSirico@users.noreply.github.com>
2022-04-16 11:19:17 +0200

    Aggiunto menu e algoritmo RE e animazioni e struct
    Menu per scegliere quale algoritmo usare, algoritmo che muove la pedina in tutte le caselle adiacenti e aggiunta delle strutture per pedina e scacchierra

--- scacchiera.c
+++ scacchiera.c
@@ -2,67 +2,230 @@
 #include <stdlib.h>
 #include <time.h>
 
-void board(FILE *,int width,int height,int DIM);
-void pedina(FILE *,int x1, int y1,int x2,int y2,int DIM);
-int main(){ 
+struct board{
+    FILE *ptrFile;
+    int width;
+    int height;
+    int DIM;
+} scacchiera;
+
+struct pawn{
+    FILE *ptrFile;
+    int x;
+    int y;
+    int DIM;
+} pedina;
+
+tempo=0;
+
+void move(struct pawn pedina,int x,int y);
+void generate_board(struct board scacchiera);
+void generate_pedina(struct pawn pedina,int x,int y);
+int main(){
     srand(time(NULL));
-    FILE *ptrFile=fopen("prova_main.svg","w");
+    FILE *ptrFile=fopen("prova.svg","w");
     //TODO controllare la se i valori sono accettabili
     //TODO calcolare la dimensione massima dei quadrati per evitare che la scacchiera esca dallo schermo
-    int width,height,DIM;
+    scacchiera.ptrFile=ptrFile;
+    pedina.ptrFile=ptrFile;
     printf("Larghezza: ");
-    scanf("%d",&width);
+    scanf("%d",&scacchiera.width);
     fflush(stdin);
     printf("Altezza: ");
-    scanf("%d",&height);
+    scanf("%d",&scacchiera.height);
     fflush(stdin);
+    //int m[width][height];
     printf("dimensioni di un rettangolo: ");
-    scanf("%d",&DIM);
-    fflush(stdin);    
-    board(ptrFile,width,height,DIM);
-    //TODO contorllare se i valori sono minori o guali alle dimensioni della scacchiera
-    int x1,y1,x2,y2;
+    scanf("%d",&scacchiera.DIM);
+    fflush(stdin);
+   
+    generate_board(scacchiera);
+    
+    //TODO controllare se i valori sono minori o guali alle dimensioni della scacchiera e non devono essere uguali
+    int x,y; //coordinate inizio e di fine
+    pedina.DIM=scacchiera.DIM;
     printf("Inserire posizione in cui si vuole iniziare: ");
-    scanf("%d %d",&x1,&y1);
+    scanf("%d %d",&pedina.x,&pedina.y);
     fflush(stdin);
     printf("Inserire posizione in cui si vuole arrivare: ");
-    scanf("%d %d",&x2,&y2);
+    scanf("%d %d",&x,&y);
     fflush(stdin);
-    pedina(ptrFile,x1,y1,x2,y2,DIM);
-    //TODO trovare posizione in px e tracciare la linea
 
+    //TODO creare un menu per sceliere il tipo di algoritmo
+    int scelta;
+    int flag;
+    generate_pedina(pedina,pedina.x,pedina.y);
+    
+    do{
+        printf("Inserire modalita' di percorso:\n");
+        printf("1. Pathfinding: tutte le direzioni\n");
+        printf("2. Pathfinding: no ostacoli\n");
+        printf("3. Pathfinding: completo\n");
+        printf("0. Esci\n");
+        printf("Scelta: ");
+        scanf("%d", &scelta);
+        fflush(stdin);
+        switch (scelta){
+            case 1:
+                FULLRe(scacchiera,pedina,x,y);
+                break;
+            case 2:
+                SEMIRe(pedina,scacchiera,x,y);
+                break;
+            case 3:
+                MovimentoConBarriere(scacchiera,pedina,x,y);
+                break;
+            case 0:
+                flag=1;
+                break;
+            default:
+                printf("\nScelta non valida!\n");
+                break;
+            }
+    }while(flag==0);
+    //TODO trovare posizione in px e tracciare la linea
+    fprintf(pedina.ptrFile,"\n</circle>");
     fprintf(ptrFile,"</svg>");
 
-    // path(width,height,x1,x2,y1,y2);
     fclose(ptrFile); 
     return 0; 
 }
 
-void pedina(FILE *ptrFile,int x1, int y1,int x2,int y2,int DIM){
+//disegna la pedina
+void generate_pedina(struct pawn pedina,int x, int y){
     //TODO sostituire il cerchio con qualcos'altro
-    fprintf(ptrFile,"\n<circle cx=\"%d\" cy=\"%d\" r=\"%d\" fill=\"red\">",x1*DIM-(DIM/2),y1*DIM+(DIM/2),DIM/2);
-    fprintf(ptrFile,"\n\n<animate attributeName=\"cx\" from=\"%d\" to=\"%d\" dur=\"5s\" fill=\"freeze\" />",x1*DIM-(DIM/2),x2*DIM-(DIM/2));
-    fprintf(ptrFile,"\n\n<animate attributeName=\"cy\" from=\"%d\" to=\"%d\" dur=\"5s\" fill=\"freeze\" />",y1*DIM-(DIM/2),y2*DIM-(DIM/2));
-
-    fprintf(ptrFile,"\n</circle>");
-
-}
-void find_path(){
+    fprintf(pedina.ptrFile,"\n<circle cx=\"%d\" cy=\"%d\" r=\"%d\" fill=\"red\">",pedina.x*pedina.DIM+(pedina.DIM/2),pedina.y*pedina.DIM+(pedina.DIM/2),pedina.DIM/2);
+    // fprintf(pedina.ptrFile,"\n\n<animate attributeName=\"cx\" from=\"%d\" to=\"%d\" dur=\"5s\" fill=\"freeze\" />",pedina.x*pedina.DIM-(pedina.DIM/2),x*pedina.DIM-(pedina.DIM/2));
+    // fprintf(pedina.ptrFile,"\n\n<animate attributeName=\"cy\" from=\"%d\" to=\"%d\" dur=\"5s\" fill=\"freeze\" />",pedina.y*pedina.DIM-(pedina.DIM/2),y*pedina.DIM-(pedina.DIM/2));
+    // fprintf(pedina.ptrFile,"\n</circle>");
 
 }
 
-void board(FILE *ptrFile,int width,int height,int DIM){
-    fprintf(ptrFile,"<svg  version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">");
-    for(int i=0;i<width;i++)
-        for(int j=0;j<height;j++)
+//muove la pedina nelle coordinate x e y
+void move(struct pawn pedina,int x,int y){
+    fprintf(pedina.ptrFile,"\n<animate attributeName=\"cx\" to=\"%d\" dur=\"1s\" begin=\"%ds\" fill=\"freeze\" />",(x*pedina.DIM)+(pedina.DIM/2),tempo);
+    fprintf(pedina.ptrFile,"\n<animate attributeName=\"cy\" to=\"%d\" dur=\"1s\" begin=\"%ds\" fill=\"freeze\" />",(y*pedina.DIM)+(pedina.DIM/2),tempo);
+    tempo=tempo+1;
+}
+
+//disegna la chess board
+void generate_board(struct board scacchiera){
+    fprintf(scacchiera.ptrFile,"<svg  version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">");
+    for(int i=0;i<scacchiera.width;i++)
+        for(int j=0;j<scacchiera.height;j++)
             if(i%2==0)
                 if(j%2==0)
-                    fprintf(ptrFile,"\n<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" style=\"fill:rgb(0,0,0)\"/>",i*DIM,j*DIM,DIM,DIM);
+                    fprintf(scacchiera.ptrFile,"\n<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" style=\"fill:rgb(0,0,0)\"/>",i*scacchiera.DIM,j*scacchiera.DIM,scacchiera.DIM,scacchiera.DIM);
                 else
-                    fprintf(ptrFile,"\n<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" style=\"fill:rgb(255,255,255)\"/>",i*DIM,j*DIM,DIM,DIM);
+                    fprintf(scacchiera.ptrFile,"\n<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" style=\"fill:rgb(255,255,255)\"/>",i*scacchiera.DIM,j*scacchiera.DIM,scacchiera.DIM,scacchiera.DIM);
             else 
                 if(j%2!=0)
-                    fprintf(ptrFile,"\n<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" style=\"fill:rgb(0,0,0)\"/>",i*DIM,j*DIM,DIM,DIM);
+                    fprintf(scacchiera.ptrFile,"\n<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" style=\"fill:rgb(0,0,0)\"/>",i*scacchiera.DIM,j*scacchiera.DIM,scacchiera.DIM,scacchiera.DIM);
                 else
-                    fprintf(ptrFile,"\n<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" style=\"fill:rgb(255,255,255)\"/>",i*DIM,j*DIM,DIM,DIM);
+                    fprintf(scacchiera.ptrFile,"\n<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" style=\"fill:rgb(255,255,255)\"/>",i*scacchiera.DIM,j*scacchiera.DIM,scacchiera.DIM,scacchiera.DIM);
+}
+
+
+//TODO movimento pedina meccanica di gioco RE in scacchi
+void FULLRe(struct board scacchiera,struct pawn pedina,int x,int y){
+    /*tabella comandi 
+    |0| |1| |2| |3| -> x
+    |1|  5   1   6
+    |2|  4   0   2
+    |3|  8   3   7
+    |
+    y*/
+    int direction,flag,c,prev;
+    flag=0;
+	prev=0;
+    c=0;
+    //generazione direzione e movimento
+    direction=rand()%8+1;
+    do{
+        switch(direction){
+            case 1:
+                if(pedina.y>0 && prev!=direction){
+                    pedina.y--;
+                    move(pedina,pedina.x,pedina.y);
+                }
+                break;
+            case 2:
+                if(pedina.x<scacchiera.width-1 && prev!=direction){
+                    pedina.x++;
+                    move(pedina,pedina.x,pedina.y);
+                }
+                break;
+            case 3:
+                if(pedina.y<scacchiera.height-1 && prev!=direction){
+                    pedina.y++;
+                    move(pedina,pedina.x,pedina.y);
+                }
+                break;
+            case 4:
+                if(pedina.x>0 && prev!=direction){
+                    pedina.x--;
+                    move(pedina,pedina.x,pedina.y);
+                }
+                break;
+            //diagonali
+            case 5:
+                if(pedina.y>0 && prev!=direction && x>0){
+                    pedina.x--;
+                    pedina.y--;
+                    move(pedina,pedina.x,pedina.y);
+                }
+                break;
+            case 6:
+                if(pedina.y>0 && prev!=direction && pedina.x<scacchiera.width-1){
+                    pedina.y--;
+                    pedina.x++;
+                    move(pedina,pedina.x,pedina.y);
+                }
+                break;
+            case 7:
+                if(pedina.y<scacchiera.height-1 && pedina.x<scacchiera.width-1 && pedina.x>0){
+                    pedina.x++;
+                    pedina.y++;
+                    move(pedina,pedina.x,pedina.y);
+                }
+            case 8:
+                if(pedina.y<scacchiera.height-1 && pedina.x>0 && prev!=direction && pedina.x>0){
+                    pedina.y++;
+                    pedina.x--;
+                    move(pedina,pedina.x,pedina.y);
+                }
+                break;
+            default:
+                printf("Error: Wrong direction generated = %d\n",direction);
+                break;
+        }
+            //direzione opposta a quella appena presa
+            switch(direction){
+                case 1:
+                    prev=direction+2;
+                    break;
+                case 2:
+                    prev=direction+2;
+                    break;				
+                case 3:
+                    prev=direction-2;
+                    break;				
+                case 4:
+                    prev=direction-2;
+                    break;
+                case 5:
+                    prev=direction+2;
+                    break;               
+                case 6:
+                    prev=direction+2;
+                    break;
+                case 7:
+                    prev=direction-2;
+                    break;                
+                case 8:
+                    prev=direction-2;
+                    break;
+           }
+		   direction=rand()%8+1;
+    }while(!(pedina.x==x && pedina.y==y));
 }
